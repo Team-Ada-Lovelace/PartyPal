ed37b077fd697ddaa367ecff357874b201a483f2
Siyun Feng <siyunf21@gmail.com>
2022-11-28 19:45:43 -0500

    added model User and updated seed.js

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,5 +1,5 @@
-const { db } = require("./database");
-const { User } = require("./models/User");
+const { db } = require('./database');
+const { User } = require('./models/User');
 
 const syncAndSeed = async () => {
   try {
@@ -7,10 +7,36 @@ const syncAndSeed = async () => {
     // this erases users everytime database starts over - DELETE BEFORE DEPLOYING
     await db.sync({ force: true });
     const users = [
-      { username: "iraisv", password: "1234" },
-      { username: "yuri", password: "abcd" },
+      {
+        username: 'iraisv',
+        password: '1234',
+        firstName: 'Irais',
+        lastName: 'Valenzuela',
+        email: 'iraisv@partypal.com',
+      },
+      {
+        username: 'yuri',
+        password: 'abcd',
+        firstName: 'Yuri',
+        lastName: 'Valenzuela',
+        email: 'yuriv@partypal.com',
+      },
+      {
+        username: 'siyun',
+        password: 'siyunf',
+        firstName: 'Siyun',
+        lastName: 'Feng',
+        email: 'siyunf@partypal.com',
+      },
+      {
+        username: 'jane',
+        password: 'janey',
+        firstName: 'Jane',
+        lastName: 'Yeh',
+        email: 'janey@partypal.com',
+      },
     ];
-    const [iraisv, yuri] = await Promise.all(
+    const [iraisv, yuri, siyun, jane] = await Promise.all(
       users.map((user) => {
         User.create(user);
       })
